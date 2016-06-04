852e0788ba23bce5bf2e15fbbfaf2087362562c9
Tim Burt <timcantango@gmail.com>
2016-06-04 13:00:40 -0500

    Complete refactor. Code runs for 2D on lattice simulation, but much more needs to be done.

--- plots.py
+++ plots.py
@@ -1,22 +1,130 @@
 import itertools
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import os
+import logging
+
+
+def check_for_folder(folder):
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
 
 def plot_two_d_random_walk_setup(tube_coords, grid_size):
+    """Plots setup and orientation of nanotubes"""
+    colors = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k'])
     tube_x = []
     tube_y = []  # x and y coordinates of tubes unzipped
     for i in range(len(tube_coords)):
         tube_x.append(tube_coords[i][0::2])
         tube_y.append(tube_coords[i][1::2])
-    colors = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k'])
     for i in range(len(tube_x)):
         plt.plot(tube_x[i], tube_y[i], c=next(colors), linestyle='--')
-    plt.xlim(-grid_size, grid_size)
-    plt.ylim(-grid_size, grid_size)
+    plt.xlim(0, grid_size)
+    plt.ylim(0, grid_size)
     plt.grid()
+    check_for_folder('plots')
     plt.savefig('plots/setup.pdf')
     plt.show()
 
 
-def two_d_path_plot(x, y):
-    plt.plot(x, y)
+def histogram_walker_2d_onlat(walker, grid_range, temp):
+    """Takes walker instance and histograms how many times location is accessed over the simulation. H not normalized
+    (for 1 walker only)"""
+    #print walker.pos[0][0]
+    walker_pos_xarr = np.zeros(len(walker.pos))
+    walker_pos_yarr = np.zeros(len(walker.pos))
+    for i in range(len(walker.pos)):
+        walker_pos_xarr[i] = walker.pos[i][0]  # converts list of lists to list of arrays
+        walker_pos_yarr[i] = walker.pos[i][1]
+    H, xedges, yedges = np.histogram2d(walker_pos_xarr, walker_pos_yarr, range=grid_range)
+    if H == 'cold':
+        H = -H  # make walker visit site negative times
+    return H, xedges, yedges
+
+
+def plot_histogram_walker_2d_onlat(walker, timesteps, H, xedges, yedges):  # plots for a single walker
+    H_norm = H / float(timesteps)  # normalizes by number of timesteps
+
+    fig = plt.figure
+
+    ax = fig.add_subplot(121)
+    ax.set_title('pcolormesh: exact bin edges')
+    X, Y = np.meshgrid(xedges, yedges)
+    ax.pcolormesh(X, Y, H_norm)
+    ax.set_aspect('equal')
+    plt.colorbar()
+
+    ax = fig.add_subplot(122)
+    ax.set_title('NonUniformImage: interpolated')
+    im = mpl.image.NonUniformImage(ax, interpolation='bilinear')
+    xcenters = xedges[:-1] + 0.5 * (xedges[1:] - xedges[:-1])
+    ycenters = yedges[:-1] + 0.5 * (yedges[1:] - yedges[:-1])
+    im.set_data(xcenters, ycenters, H)
+    ax.images.append(im)
+    ax.set_xlim(xedges[0], xedges[-1])
+    ax.set_ylim(yedges[0], yedges[-1])
+    ax.set_aspect('equal')
+    plt.colorbar()
     plt.show()
+    return H, xedges, yedges
+
+
+def plot_histogram_walkers_2d_onlat(walker, timesteps, num_walkers, H_tot, xedges, yedges):  # plots for all walkers
+    H_norm = H_tot / (float(num_walkers)*float(timesteps))
+
+    test = sum(sum(H_norm))
+    correction = -1.0/test
+    H_norm *= correction
+    new_h = sum(sum(H_norm))
+    logging.info('Plot is normalized')
+    fig = plt.figure()
+    #if option == 'equidistant':
+     #   plt.title('imshow: equidistant')
+    im = plt.imshow(H_norm, interpolation='nearest', origin='low',
+                        extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
+        #plt.colorbar()
+
+    #ax = fig.add_subplot(132)
+    plt.title('pcolormesh: exact bin edges')
+    X, Y = np.meshgrid(xedges, yedges)
+    plt.pcolormesh(X, Y, H_norm.T)  # transpose since pcolormesh reverses axes
+    plt.xlim(xedges[0], xedges[-1])
+    plt.ylim(yedges[0], yedges[-1])
+    plt.colorbar()
+    plt.show()
+
+    # ax = fig.add_subplot(133)
+    # ax.set_title('NonUniformImage: interpolated')
+    # im = mpl.image.NonUniformImage(ax, interpolation='bilinear')
+    # xcenters = xedges[:-1] + 0.5 * (xedges[1:] - xedges[:-1])
+    # ycenters = yedges[:-1] + 0.5 * (yedges[1:] - yedges[:-1])
+    # im.set_data(xcenters, ycenters, H_tot)
+    # ax.images.append(im)
+    # ax.set_xlim(xedges[0], xedges[-1])
+    # ax.set_ylim(yedges[0], yedges[-1])
+    # ax.set_aspect('equal')
+    # plt.colorbar()
+    # plt.show()
+
+
+def plot_walker_path_all(walkers_pos, grid_size):  # plot all paths for all time steps
+    colors = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k'])
+    print len(walkers_pos)
+    print len(walkers_pos[-1])
+    pos = np.array((len(walkers_pos),len(walkers_pos[-1])))
+    print pos.size
+    for i in range(len(walkers_pos[-1])):  # one plot for each walker
+        coord = []
+        for j in range(len(walkers_pos)):  # runs over timesteps
+           coord.append(walkers_pos[j][i].pos)
+        # coord now holds list of tuples for one walker at every timestep
+        print coord
+        plt.plot(coord,c=next(colors))
+    plt.xlim(-grid_size, grid_size)
+    plt.ylim(-grid_size, grid_size)
+    plt.grid()
+    plt.savefig('plots/walker_path_all.pdf')
+    plt.show()
+
--- run.py
+++ run.py
@@ -1,55 +1,102 @@
-import numpy as np
-import argparse
 import plots
-import setup
+import creation
 import randomwalk
+import ConfigParser
+import os
+import logging
+# from mpi4py import MPI
+# import platform
+# import multiprocessing as mp
 
 
-def yesno():
-    response = raw_input('    Continue? (y/n) ')
-    if len(response) == 0:  # [CR] returns true
-        return True
-    elif response[0] == 'n' or response[0] == 'N':
-        return False
-    else:  # Default
-        return True
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                print("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+
+def logging_setup(save_dir):
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%m-%d %H:%M',
+                        filename='%s/run.log' % save_dir,
+                        filemode='w')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='.')
-    parser.add_argument('--grid_size', type=int, default=100, help='Size of square grid of use')
-    parser.add_argument('--tube_length', type=float, default=20, help='Length of nanotube')
-    parser.add_argument('--num_tubes', type=int, default=100, help='How many tubes are there for random walker to use')
-    parser.add_argument('--mean_dist_tubes', type=int, default=40,
-                        help='Mean distance between centers of tubes')
-    parser.add_argument('--exact_sol', type=bool, default=False, help='Exact integer solutions used when calculating '
-                                                                      'positions of nanotubes. Otherwise x and y values'
-                                                                      ' rounded.')
-    parser.add_argument('--orientation', type=str, default='random', help='Orientation of nanotubes in medium. random, '
-                                                                          'horizontal, vertical, or angle in DEGREES')
-    parser.add_argument('--walker_start_dir', type=str, default='left', help='Direction positive walkers come in from. '
-                                                                         'top, bottom, left, right for 2D or '
-                                                                         'Miller Index for 3D')
-    parser.add_argument('--num_walkers', type=int, default=50, help='How many total walkers (pos and neg) are used')
-    parser.add_argument('--timesteps', type=int, default=50, help='How many steps to run the simulation for')
-    parser.add_argument('--bd_condition', type=str, default='exit', help='What happens when walkers hit the wall.'
-                                                                               'reflect - isothermal,'
-                                                                            'exit - constant flux')
-    args = parser.parse_args()
+    Config = ConfigParser.ConfigParser()
 
-    tube_length = args.tube_length
-    grid_size = args.grid_size
-    num_tubes = args.num_tubes
-    exact_sol = args.exact_sol
-    orientation = args.orientation
-    num_walkers = args.num_walkers
-    mean_dist_tubes = np.abs(args.mean_dist_tubes)
-    walker_start_dir = args.walker_start_dir
-    bd_condition = args.bd_condition
-    timesteps = args.timesteps
+    if os.path.exists("config.ini"):
+        Config.read("config.ini")
+        config_used = 'Using config.ini'
+    else:
+        Config.read("default.ini")
+        config_used = 'Using default.ini'
 
-    grid = setup.Grid2D(grid_size, tube_length, num_tubes, mean_dist_tubes, exact_sol, orientation)
-    plots.plot_two_d_random_walk_setup(grid.tube_coords, grid_size)
-    walkers_pos = setup.setupwalkers2d(num_walkers, grid_size, walker_start_dir) # list of walker object list
-    #  at every timestep
-    randomwalk.runrandomwalk2d(walkers_pos, grid_size, timesteps, bd_condition, walker_start_dir)
+    dim = Config.getint('config','dim')
+    grid_size = Config.getint('config','grid_size')
+    orientation = Config.get('config','orientation')
+    tube_length = Config.getfloat('config','tube_length')
+    num_tubes = Config.getint('config','num_tubes')
+    on_lattice = Config.getboolean('config','on_lattice')
+    num_walkers = Config.getint('config','num_walkers')
+    timesteps = Config.getint('config','timesteps')
+    save_dir = Config.get('config', 'save_dir')
+    #mean_dist_tubes = Config.get('config','mean_dist_tubes')
+    #std_dist_tubes = Config.get('config', 'std_dist_tubes')
+
+    logging_setup(save_dir)
+    logging.info(config_used)
+
+    os.chdir(save_dir)
+    logging.info("Setting up grid and tubes")
+    grid = creation.Grid2D_onlat(grid_size, tube_length, num_tubes, orientation)
+    plots.plot_two_d_random_walk_setup(grid.tube_coords, grid.size)
+
+    # logging.info("Initializing MPI")
+    # comm = MPI.COMM_WORLD
+    # rank = comm.Get_rank()
+    # size = comm.Get_size()
+    # machine = platform.node()
+    # print("Hello MPI from %s %d of %d" % (machine, rank, size))
+
+    grid_range = [[0, grid.size], [0, grid.size]]
+
+    for i in range(num_walkers):
+        # run hot walkers
+        logging.info("Start hot walker %d out of %d" % (i+1,num_walkers))
+        walker = randomwalk.runrandomwalk_2d_onlat(grid, timesteps, 'hot')
+        if i == 0:
+            H, xedges, yedges = plots.histogram_walker_2d_onlat(walker, grid_range, 'hot')
+            # bin_edges should stay consistent since range is fixed
+        else:
+            H_temp, xedges, yedges = plots.histogram_walker_2d_onlat(walker, grid_range, 'hot')
+            H += H_temp
+
+    for i in range(num_walkers):
+        # run cold walkers
+        logging.info("Start cold walker %d out of %d" % (i+1,num_walkers))
+        walker = randomwalk.runrandomwalk_2d_onlat(grid, timesteps, 'cold')
+        if i == 0:
+            H, xedges, yedges = plots.histogram_walker_2d_onlat(walker, grid_range, 'cold')
+            # bin_edges should stay consistent since range is fixed
+        else:
+            H_temp, xedges, yedges = plots.histogram_walker_2d_onlat(walker, grid_range, 'cold')
+            H -= H_temp
+
+    logging.info("Finished random walks")
+    plots.plot_histogram_walkers_2d_onlat(walker, timesteps, num_walkers, H, xedges, yedges)
 
