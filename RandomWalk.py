1477734dccf4dd483ab9e2710b22691912eb35dc
Tim Burt <timcantango@gmail.com>
2016-06-04 13:05:35 -0500

    Complete refactor. Code runs for 2D on lattice simulation, but much more needs to be done.

--- RandomWalk.py
+++ RandomWalk.py
@@ -1,52 +1,64 @@
 import numpy as np
-from mpi4py import MPI
-import setup
+import creation
+import logging
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-
-
-def runrandomwalk2d(walkers_pos, grid_size, timesteps, bd_condition, walker_start_dir):
-    # Start the random walk
-    step = 0
+def runrandomwalk_2d_onlat(grid, timesteps, temp):
+    # Start the random walk, for one walker
+    walker = creation.Walker2D_onlat(grid.size, temp)
     moves = [[0, 1], [1, 0], [0, -1], [-1, 0]]
     for i in range(1,timesteps+1):
-        step += 1
-        print "On step {0}".format(step)
-        d_pos = []
-        for j in range(len(walkers_pos)):
-            d_pos.append(moves[np.random.randint(0, 3)])
-            # array of all new positions for all walkers
-        updatewalkers2d(walkers_pos, d_pos)
-        applybdconditions(walkers_pos, bd_condition, grid_size, walker_start_dir)
-    print 'Done in %d steps' % step
-    return walkers_pos
+        d_pos = moves[np.random.randint(0, 4)]
+        walker.add_dpos(d_pos)
+        walker = applytubejumps2d(walker, grid)
+        walker = applybdconditions2d(walker, grid)
+    logging.info('Done in %d steps' % i)
+    return walker
 
 
-def updatewalkers2d(pos_all, newpos):
-    pos_all.append(pos_all[-1])  # copies last timestep to current one
-    for i in range(len(pos_all[-1])):
-        pos_all[-1][i].updatepos(newpos)
-    return pos_all
+def applytubejumps2d(walker, grid):
+    # This code is slow, refactor to reduce load.
+    jump_moves = [[0, 1], [1, 0], [0, -1], [-1, 0],[0, 1], [1, 0], [0, -1], [-1, 0]]
+    # 8 possible jump positions for now if at tube end, 4 at left (first 4) and 4 at right (second 4)
+    tube_l = []
+    tube_r = []  # left and right coords of tubes (x,y)
+    for i in range(len(grid.tube_coords)):
+        tube_l.append(grid.tube_coords[i][0:2])
+        tube_r.append(grid.tube_coords[i][2:4])
+    cur_pos = list(walker.pos[-1])
+    cur_pos_rev = list(np.flipud(walker.pos[-1]))
+    for i in range(len(tube_l)):
+        if tube_l[i] == (cur_pos or cur_pos_rev):  # coord on left tube end jumps to right end
+            logging.debug("Jump found")
+            choice = np.random.randint(0, 8)
+            d_pos = jump_moves[choice]
+            if choice <= 3:  # stay at left end
+                walker.replace_dpos(d_pos)
+            else:  # jump across tube to right end
+                newpos = np.array(tube_r[i]) + np.array(d_pos)
+                walker.replace_pos(newpos)
+        elif tube_r[i] == (cur_pos or cur_pos_rev):  # coord on left tube end jumps to right end
+            logging.debug("Jump found")
+            choice = np.random.randint(0, 8)
+            d_pos = jump_moves[choice]
+            if choice <= 3:  # stay at left end
+                walker.replace_dpos(d_pos)
+            else:  # jump across tube to right end
+                newpos = np.array(tube_l[i]) + np.array(d_pos)
+                walker.replace_pos(newpos)
+    return walker
 
 
-def applybdconditions(pos_all, bd_condition, grid_size, walker_start_dir):
-    for i in range(len(pos_all[-1])):
-        cur_pos = pos_all[-1][i].pos
-        if bd_condition == 'reflect':
-            if cur_pos[0] > grid_size:  # x coordinate
-                cur_pos[0] = grid_size
-            elif cur_pos[0] < -grid_size:
-                cur_pos[0] = -grid_size
-            if cur_pos[1] > grid_size: # y coordinate
-                cur_pos[1] = grid_size
-            elif cur_pos[1] < -grid_size:
-                cur_pos[1] = -grid_size
-            pos_all[-1][i].updatepos(cur_pos)
-        elif bd_condition == 'exit':
-            pos_all[-1][i] = setup.Walker2D(grid_size, walker_start_dir) # starts back at random origin point
-        else:
-            print('Check bd condition argument.')
-            raise SystemExit
-    return pos_all
-            
+def applybdconditions2d(walker, grid):
+    # periodic bd conditions for y=0 and y=grid.size
+    # reflective bd conditions for x=0 and x=grid.size
+    cur_pos = walker.pos[-1]
+    if cur_pos[0] == grid.size:  # x coordinate
+        cur_pos[0] -= 1
+    elif cur_pos[0] == 0:
+        cur_pos[0] += 1
+    if cur_pos[1] > grid.size: # y coordinate
+        cur_pos[1] = 1
+    elif cur_pos[1] < 0:
+        cur_pos[1] = grid.size-1
+    walker.replace_pos(cur_pos)
+    return walker
---
+++ creation.py
@@ -0,0 +1,103 @@
+import numpy as np
+import logging
+
+class Grid2D_onlat(object):
+    def __init__(self, grid_size, tube_length, num_tubes, orientation):
+        """Grid in first quadrant only for convenience"""
+        self.size = grid_size
+        if tube_length > grid_size:
+            logging.error('Nanotube is too large for grid')
+            raise SystemExit
+        self.tube_coords = []
+        self.tube_centers = []
+        if num_tubes > 0:  # tubes exist
+            for i in range(num_tubes):  # currently no mean dist used, ADD LATER?
+                x_l, y_l, x_r, y_r, x_c, y_c = self.generate_2d_tube(tube_length, orientation)
+                self.tube_centers.append([x_c, y_c])
+                self.tube_coords.append([x_l, y_l, x_r, y_r])
+
+    def generate_2d_tube(self, radius, orientation):
+        """Finds appropriate angles within one degree that can be chosen from for random, should be good enough"""
+        # first generate a left endpoint anywhere in the box
+        x_l = np.random.randint(0, self.size + 1)
+        y_l = np.random.randint(0, self.size + 1)
+        good_angles = []
+        if orientation == 'random':
+            angle_range = range(0,360)
+        elif orientation == 'vertical':
+            angle_range = [90, 270]
+        elif orientation == 'horizontal':
+            angle_range = [0, 180]
+        for i in angle_range:
+            x_test = radius * np.cos(np.deg2rad(i)) + x_l
+            y_test = radius * np.sin(np.deg2rad(i)) + y_l
+            if (x_test >= 0) & (x_test <= self.size) & (y_test >= 0) & (y_test <= self.size):
+                # angle and x_l choice inside box
+                good_angles.append(i)
+        angle = np.random.choice(good_angles)  # in degrees
+        x_r = radius * np.cos(np.deg2rad(angle)) + x_l
+        y_r = radius * np.sin(np.deg2rad(angle)) + y_l
+        x_c = (radius/2.0) * np.cos(np.deg2rad(angle)) + x_l
+        y_c = (radius/2.0) * np.sin(np.deg2rad(angle)) + y_l
+        return x_l, y_l, x_r, y_r, x_c, y_c
+
+
+def taxicab_dist(x0, y0, x1, y1):
+    dist = np.abs(x1 - x0) + np.abs(y1 - y0)
+    return dist
+
+
+def radius(x, y):
+    radius = np.sqrt(x ** 2 + y ** 2)
+    return radius
+
+
+def euc_dist(x0, y0, x1, y1):
+    dist = np.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+    return dist
+
+
+class Walker2D_onlat(object):
+    def __init__(self, grid_size, temp):
+        if temp == 'hot':
+            start_x = 0
+        elif temp == 'cold':
+            start_x = grid_size
+        else:
+            logging.error('Invalid walker temperature')
+            raise SystemExit
+        start_y = np.random.randint(0, grid_size+1)
+        start = [start_x, start_y]
+        self.pos = [start]
+
+    def add_dpos(self, newpos):  # add incremented position
+        arr1 = np.array(self.pos[-1])
+        arr2 = np.array(newpos)
+        #print arr1, arr2
+        self.pos.append(list(arr1 + arr2))
+
+    def add_pos(self, newpos):  # add new position
+        self.pos.append(list(newpos))
+
+    def replace_pos(self, newpos):  # replace current position
+        self.pos[-1] = list(newpos)
+
+    def replace_dpos(self, newpos):  # replace incremented position
+        arr1 = np.array(self.pos[-1])
+        arr2 = np.array(newpos)
+        self.pos[-1] = list(arr1 + arr2)
+
+
+def int_on_circle(radius):  # finds all integer solutions on the circumference of a circle
+    # centered at origin for a given radius
+    maxlegdist = int(np.floor(radius))
+    sols = []
+    for i in range(maxlegdist + 1):
+        test = np.sqrt(radius ** 2 - i ** 2)
+        sol_test = test.is_integer()
+        if sol_test == True:
+            sols.append((i, int(test)))  # stores all x,y integer solutions as tuples
+            sols.append((i, -int(test)))
+            sols.append((-i, int(test)))
+            sols.append((-i, -int(test)))
+    return sols
