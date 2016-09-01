Fixed incorrect heat flux calculation

conduction/onlat_3d_constant_flux.py
@@ -241,7 +241,7 @@ def parallel_method(grid_size, tube_length, tube_radius, num_tubes, orientation,
             # print np.count_nonzero(H_master)
             dt_dx, heat_flux, gradient_err, k, k_err, r2, temp_profile_sum = analysis.check_convergence_3d_onlat(
                 H_master, cur_num_walkers,
-                grid.size, core_time)
+                grid.size, core_time + 1)
             k_list.append(k)
             dt_dx_list.append(dt_dx)
             heat_flux_list.append(heat_flux)