7d51f97441a9032c3cae284fd1f1a16236563161
Tim Burt <timcantango@gmail.com>
2016-09-02 02:53:28 -0500

    Trying new timestep formula

--- conduction/onlat_2d_constant_flux.py
+++ conduction/onlat_2d_constant_flux.py
@@ -240,10 +240,11 @@ def parallel_method(grid_size, tube_length, tube_radius, num_tubes, orientation,
             H_local[cold_temp_pos[0], cold_temp_pos[1]] -= 1
             # send to core 0
             # as long as size is somewhat small, this barrier won't slow things down much and ensures a correct k value
+            comm.Barrier()
         comm.Barrier()
         comm.Reduce(H_local, H_master, op=MPI.SUM, root=0)
         # analysis
-        cur_num_walkers = 2 * (i + 1) * size
+        cur_num_walkers = i * size
         if rank == 0 and (i > 0):
             # print np.count_nonzero(H_master)
             dt_dx, heat_flux, dt_dx_err, k, k_err, r2 = analysis.check_convergence_2d_onlat(H_master, cur_num_walkers,
