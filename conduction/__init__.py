536f81cfb43262eed7cc7d58d8fac7b32da396fd
Tim <taburt@ou.edu>
2017-07-29 06:37:37 -0500

    Bug fixes in 3D Bresenham's Algorithm (4-connect like (orthogonal)) and random fixes.

--- conduction/__init__.py
+++ conduction/__init__.py
@@ -1,14 +1,13 @@
-
-from conduction import analysis
-from conduction import backend
-from conduction import creation_2d
-from conduction import creation_3d
-from conduction import mpi_run
-from conduction import randomwalk_2d
-from conduction import randomwalk_3d
-from conduction import plots
-from conduction import rules_2d
-from conduction import rules_3d
-from conduction import run
-from conduction import test_2d
-from conduction import test_3d
+# from conduction import analysis
+# from conduction import backend
+# from conduction import creation_2d
+# from conduction import creation_3d
+# from conduction import mpi_run
+# from conduction import randomwalk_2d
+# from conduction import randomwalk_3d
+# from conduction import plots
+# from conduction import rules_2d
+# from conduction import rules_3d
+# from conduction import run
+# from conduction import test_2d
+# from conduction import test_3d
