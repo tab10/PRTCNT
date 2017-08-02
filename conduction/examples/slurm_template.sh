b84b56259bf7d18e25a3477367eedf8c9b796229
Tim <taburt@ou.edu>
2017-08-01 20:52:40 -0500

    Updated SLURM template to currently run on Schooner

--- conduction/examples/slurm_template.sh
+++ conduction/examples/slurm_template.sh
@@ -1,22 +1,21 @@
-#!/bin/sh -login
-#SBATCH --job-name conduction
+#!/bin/bash
 #SBATCH --partition=normal
-#SBATCH --ntasks=80
+#SBATCH --job-name conduction
+#SBATCH --ntasks=200
 #SBATCH --ntasks-per-node=20
-#SBATCH -t 06:00:00
+#SBATCH -t 00:10:00
 
 module load mpi4py/1.3.1-intel-2015b-Python-2.7.10
+module load numpy/1.10.4-intel-2016b-Python-2.7.10
 module load matplotlib/1.5.0-intel-2016a-Python-2.7.10
-module load numpy/1.10.4-intel-2016a-Python-2.7.10
+module load scipy/0.17.0-intel-2016a-Python-2.7.12
 
 for i in {1..1}; do
-mpirun python /home/tab10/code/conduction/conduction/mpi_run.py --dim 3 --orientation horizontal --num_tubes X --tube_length 15 --gen_plots True --rules_test True
+mpirun python /home/tab10/code/conduction/conduction/mpi_run.py --dim 3 --orientation random --prob_m_cn 0.5 --num_tubes X --tube_length 15 --gen_plots True --num_walkers 210000 --timesteps 35000 --model kapitza
 done
 
-# for linux
-mpirun -np 2 python /Users/tim/Documents/PycharmProjects/conduction/conduction/mpi_run.py --dim 3 --orientation random --num_tubes 10 --tube_length 10 --gen_plots True --rules_test False --timesteps 15000 --num_walkers 60000 --model kapitza --grid_size 30 --prob_m_cn 0.5
+# for local mac/linux
+#mpirun -np 2 python /Users/tim/Documents/PycharmProjects/conduction/conduction/mpi_run.py --dim 3 --orientation random --num_tubes 10 --tube_length 10 --gen_plots True --rules_test False --timesteps 15000 --num_walkers 60000 --model kapitza --grid_size 30 --prob_m_cn 0.5
 
-# for windows
-mpiexec -n 2 python C:\Users\Tim\Documents\PycharmProjects\conduction\conduction\mpi_run.py --dim 3 --orientation random --num_tubes 1 --tube_length 10 --gen_plots True --rules_test True --timesteps 15000 --num_walkers 250 --model kapitza --grid_size 20 --prob_m_cn 0.5 --disable_func False
-
-mpirun -np 2 python mpi_run.py --dim 3 --orientation random --num_tubes 1 --tube_length 10 --gen_plots True --rules_test True --timesteps 35000 --num_walkers 50 --model kapitza --prob_m_cn 0.5 --grid_size 30
+# for local windows
+#mpiexec -n 2 python C:\Users\Tim\Documents\PycharmProjects\conduction\conduction\mpi_run.py --dim 3 --orientation random --num_tubes 1 --tube_length 10 --gen_plots True --rules_test True --timesteps 15000 --num_walkers 250 --model kapitza --grid_size 20 --prob_m_cn 0.5 --disable_func False
